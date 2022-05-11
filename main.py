208f541a7feb4ac1295e7087966924bfc9e28b58
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 <dustin@nayya.com>
2022-05-11 16:03:39 -0400

    first

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import json
+import requests
+
+auth_token = os.environ.get('SCIMTOKEN')
+url = os.environ.get('URL')
+headers = {'Authorization': 'Bearer {}'.format(auth_token)}
+
+response = requests.get(url + '/Users', headers=headers)
+
+print(response.status_code)
+print(response.url)
+print(response.headers)
+content = json.loads(response.text)
+for users in content['Resources']:
+    print(users['id'] + " " + users['userName'])
+
+print("Status Code", response.status_code)
+print(content['totalResults'])
+print(content['itemsPerPage'])
